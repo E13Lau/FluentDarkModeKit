c3b54a1533900bc83b5d850dfd872f899a06be1d
E13 <chn.lyzasd@gmail.com>
2021-02-03 17:42:43 +0800

    support iOS 10

--- Sources/DarkModeCore/UIView+DarkModeKit.h
+++ Sources/DarkModeCore/UIView+DarkModeKit.h
@@ -14,8 +14,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UIView (DarkModeKit) <DMTraitEnvironment>
 
-- (void)dm_updateDynamicColors API_DEPRECATED("dm_updateDynamicColors is deprecated and will not be called on iOS 13.0, use dmTraitCollectionDidChange: instead", ios(11.0, 13.0));;
-- (void)dm_updateDynamicImages API_DEPRECATED("dm_updateDynamicImages is deprecated and will not be called on iOS 13.0, use dmTraitCollectionDidChange: instead", ios(11.0, 13.0));;
+- (void)dm_updateDynamicColors API_DEPRECATED("dm_updateDynamicColors is deprecated and will not be called on iOS 13.0, use dmTraitCollectionDidChange: instead", ios(10.0, 13.0));;
+- (void)dm_updateDynamicImages API_DEPRECATED("dm_updateDynamicImages is deprecated and will not be called on iOS 13.0, use dmTraitCollectionDidChange: instead", ios(10.0, 13.0));;
 
 @end
 
